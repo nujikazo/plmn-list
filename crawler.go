e9ef85d03b958bb3ea62d65c4fff5a24a01cb1c0
nujikazo <nujikazo@gmail.com>
2022-03-31 14:29:59 +0900

    fix

--- crawler.go
+++ crawler.go
@@ -6,12 +6,31 @@ import (
 
 	"github.com/nujikazo/plmn-list/crawl"
 	"github.com/nujikazo/plmn-list/crawl/config"
+	"github.com/nujikazo/plmn-list/crawl/database"
 )
 
 func main() {
 	generalConf := config.ReadGeneralConf(os.Getenv("GENERAL_CONF"))
 	crawlerConf := config.ReadPlmnCrawlConf(os.Getenv("CRAWLER_CONF"))
-	if err := crawl.Run(generalConf, crawlerConf); err != nil {
+	if err := run(generalConf, crawlerConf); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run(generalConf *config.GeneralConf, crawlerConf *config.PlmnCrawlConf) error {
+	db, err := database.New(generalConf)
+	if err != nil {
+		return err
+	}
+
+	list, err := crawl.Run(generalConf, crawlerConf)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Upsert(list); err != nil {
+		return err
+	}
+
+	return nil
 }
